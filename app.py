49faab2bf3c302c7641b13604e5fa89a7492e0ec
Artem Fedorov <artem.fedorov@dataart.com>
2022-06-10 22:48:17 +0300

    fix wrong import

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from aiohttp import web_routedef
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from starlette import status
@@ -14,8 +13,8 @@ app.include_router(routes.router)
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(
-        _request: web_routedef.Request,
-        exc: RequestValidationError  # handle validation errors as 400 BAd request instead of 422
+    _request: Request,
+    exc: RequestValidationError,  # handle validation errors as 400 BAd request instead of 422
 ):
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
